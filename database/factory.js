b7530285cc24f2094716736b4085b1874aecc808
fabysshk <fabysshk@gmail.com>
2020-12-11 17:52:37 +0200

    delpoy

--- database/factory.js
+++ database/factory.js
@@ -1,17 +1,4 @@
-'use strict'
-
-/*
-|--------------------------------------------------------------------------
-| Factory
-|--------------------------------------------------------------------------
-|
-| Factories are used to define blueprints for database tables or Lucid
-| models. Later you can use these blueprints to seed your database
-| with dummy data.
-|
-*/
-
-/** @type {import('@adonisjs/lucid/src/Factory')} */
+"use strict";
 // const Factory = use('Factory')
 
 // Factory.blueprint('App/Models/User', (faker) => {
